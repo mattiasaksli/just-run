ddb8de1a2620a5bdf7f2b22051c23703726adcf6
KristoferKSR <kaosaarkristofer@gmail.com>
2020-12-14 19:45:48 +0200

    Merge

--- app/src/main/java/com/example/justrun/activities/MapsActivity.kt
+++ app/src/main/java/com/example/justrun/activities/MapsActivity.kt
@@ -138,7 +138,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SensorEventListene
 
         Log.i("workoutData", workoutData.toString())
         if (SettingsActivity.SWITCH_DATA) db.workoutDataDao().insert(workoutData)
-
         val returnIntent = intent
         returnIntent.putExtra("maps", "finished")
         setResult(Activity.RESULT_OK, returnIntent)
